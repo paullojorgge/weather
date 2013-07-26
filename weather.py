23ffa1182bc9b5d7c0c2f0a967d3de7cc2439313
Paulo Jorge <paullojorgge@gmail.com>
2013-07-26 16:45:40 +0100

    update comments

--- weather.py
+++ weather.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 # My first Python test
-# weather.py - ask for a city and display the actual temperature
+# weather.py - ask for a city and display the actual temperature/hour
 
 # Modules
 import urllib2
@@ -17,14 +17,15 @@ hour = url.info()['date'] # get the time of the query
 html = url.read()  # get all data
 raw = nltk.clean_html(html) # clean the data
 #res2 = re.search('(?<=Paris )(.{17})', raw).group() # info by city
-res = re.search('(?<=' + re.escape(city) + ')(.{50})', raw) # info by city
-data = res.group().split() # convert res to a list
-temp = int(float(data[4])) # convert temperature to number
+# info using the input city
+res = re.search('(?<=' + re.escape(city) + ')(.{50})', raw)
+# save res to a list
+data = res.group().split()
+# convert temperature to a number - was a string
+temp = int(float(data[4]))
+# convert Fahrenheit to Celsius
 temperature = round(float(temp-32)/9*5,2)
-#print temperature
-#print data
-#print res2
-print "##############################"
+print "###################################"
 print "City:", city
 print "Time:", data[1], data[2], data[3]
 print "Temperature(Celsius):", temperature #, unichr(186), "C"
